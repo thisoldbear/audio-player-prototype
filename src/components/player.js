7cfc577635de9caeb2db870c15d906271a89c82b
thisoldbear <danielwilliams.design@gmail.com>
2019-04-25 21:44:08 +0100

    Add skip audio

--- src/components/player.js
+++ src/components/player.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 
-export default () => {
-  return (
-    <audio controls>
-      {/* http://freemusicarchive.org/music/Pudge/ */}
-      <source src="Pudge_-_02_-_Here_Comes_The_Science_Bit.mp3" type="audio/mpeg" />
-      Your browser does not support the audio element.
-    </audio>
-  )
+class Player extends Component {
+  constructor(props) {
+    super(props);
+    this.audio = React.createRef();
+  }
+
+  state = {
+    // http://freemusicarchive.org/music/Pudge/
+    src: 'Pudge_-_02_-_Here_Comes_The_Science_Bit.mp3'
+  }
+
+  skipAudio = () => {
+    this.setState({
+      // http://www.obamadownloads.com/obama-mp3s.html
+      src: 'victory-speech.mp3'
+    }, () => {
+      this.refs.audio.play();
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <audio controls ref="audio" key={this.state.src}>
+          <source src={this.state.src} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+        <button onClick={() => {
+          this.skipAudio()
+        }}>Skip</button>
+      </div>
+    )
+  }
 };
+
+export default Player;
