5edbc8d75b8b90428b75a3b65aba3b14cd8fd4c7
thisoldbear <danielwilliams.design@gmail.com>
2019-04-26 08:15:03 +0100

    Add basic JS controls

--- src/components/player.js
+++ src/components/player.js
@@ -1,14 +1,16 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react'
 
 class Player extends Component {
   constructor(props) {
-    super(props);
-    this.audio = React.createRef();
+    super(props)
+    this.audio = React.createRef()
   }
 
   state = {
     // http://freemusicarchive.org/music/Pudge/
-    src: 'Pudge_-_02_-_Here_Comes_The_Science_Bit.mp3'
+    src: 'Pudge_-_02_-_Here_Comes_The_Science_Bit.mp3',
+    volume: 0.5,
+    playing: false,
   }
 
   skipAudio = () => {
@@ -16,23 +18,75 @@ class Player extends Component {
       // http://www.obamadownloads.com/obama-mp3s.html
       src: 'victory-speech.mp3'
     }, () => {
-      this.refs.audio.play();
+      this.refs.audio.play()
     })
+  }
+
+  playAudio = () => {
+    if (this.state.playing) {
+      this.refs.audio.pause()
+    } else {
+      this.refs.audio.play()
+    }
+
+    this.setState((state) => ({
+      ...state,
+      playing: !state.playing
+    }))
+  }
+
+  handleVolumeChange = (value) => {
+    this.refs.audio.volume = value
+  }
+
+  componentDidMount() {
+    this.refs.audio.volume = this.state.volume
   }
 
   render() {
     return (
-      <div>
-        <audio controls ref="audio" key={this.state.src}>
+      <div className="player">
+        <audio ref="audio">
           <source src={this.state.src} type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
-        <button onClick={() => {
-          this.skipAudio()
-        }}>Skip</button>
+        <div>
+          <button onClick={() => {
+            this.playAudio()
+          }}>{this.state.playing ? '⏸' : '▶'}</button>
+        </div>
+        <div>
+          🔈
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            value={this.state.volume}
+            onChange={(e) => {
+              const value = e.target.value
+
+              this.setState((state) => ({
+                ...state,
+                volume: value,
+              }))
+
+              this.handleVolumeChange(value)
+            }
+            }
+          />
+          {
+            this.state.volume * 100
+          }
+        </div>
+        <div>
+          <button onClick={() => {
+            this.skipAudio()
+          }}>Skip</button>
+        </div>
       </div>
     )
   }
-};
+}
 
-export default Player;
+export default Player
